8315439c236e7eb176294742c0f585141e4b5e36
Marcus Mouyat <marcus@mouyat.com>
2019-07-24 12:02:12 +1000

    Added files to repository

---
+++ Assets/Scripts/TowerSelector.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerSelector : MonoBehaviour
+{
+
+    //public GameObject tower;
+
+    public int[] availableTowers;
+
+    public int selectedTower = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit click;
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(mouseRay, out click))
+            {
+                Placeable p = click.collider.GetComponent<Placeable>();
+
+                if (p.isOccupied == false)
+                {
+                    SpawnTower(availableTowers[selectedTower], p.transform.position);
+                }
+
+            }
+        }
+    }
+
+    public void SpawnTower(int towerNumber, Vector3 position)
+    {
+        GameObject towerPrefab = Resources.Load<GameObject>("Prefabs/Towers/Tower " + towerNumber);
+        Instantiate(towerPrefab, position, Quaternion.identity);
+    }
+}
