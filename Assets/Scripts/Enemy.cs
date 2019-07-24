39abad50f1080c700a002674fab68905bda6e55f
Marcus Mouyat <marcus@mouyat.com>
2019-07-24 16:15:07 +1000

    an absolute mess of broken code

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 100;
+    public int damage = 10;
+    public float speed = 10f;
+    
+
+    protected List<Transform> DetectObstacles()
+    {
+
+        return new List<Transform>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damageTaken)
+    {
+        health -= damageTaken;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public int damage = 10;
+    public float fireDelay = 1;
+    public float range = 10;
+    public int cost = 100;
+    public int level = 1;
+
+    float fireTimer;
+
+
+    /*
+    protected List<Transform> DetectTargets()
+    {
+
+    }
+    */
+
+    public Transform GetClosestTarget(List<Transform> targets)
+    {
+        float min = float.MaxValue;
+        Transform closest = null;
+        foreach (var target in targets)
+        {
+
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+
+
+    }
+
+    public virtual void Aim()
+    {
+
+    }
+
+    public virtual void Fire()
+    {
+
+    }
+}
